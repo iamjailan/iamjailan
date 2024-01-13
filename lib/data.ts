be1f43bc50d4899bcdd19fa1fa369629ce950edd
Jailan Mirkhel <ahmadjailan2012@gmail.com>
2024-01-13 09:36:54 +0430

    creating static data

--- lib/data.ts
+++ lib/data.ts
@@ -35,26 +35,34 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Full-Stack Web Developer",
-    location: "Kabul, Afghanistan",
+    title: "Graduated bootcamp",
+    location: "Miami, FL",
     description:
-      "Full Stack Next.js Developer in MCCO Organization, i created a Full-Stack website using Next 14, Tailwind CSS and MongoDB",
+      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
     icon: React.createElement(LuGraduationCap),
-    date: "2023",
+    date: "2019",
   },
   {
-    title: "Backend Developer",
-    location: "Kabul, Afghanistan",
+    title: "Front-End Developer",
+    location: "Orlando, FL",
     description:
-      "I worked as Backend Developer in Aseel Organizations, our jobs is to created E-commerce Backend for Aseel App using MedusaJS.",
+      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
     icon: React.createElement(CgWorkAlt),
-    date: "2023 - In Progress",
+    date: "2019 - 2021",
+  },
+  {
+    title: "Full-Stack Developer",
+    location: "Houston, TX",
+    description:
+      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+    icon: React.createElement(FaReact),
+    date: "2021 - present",
   },
 ] as const;
 
 export const projectsData = [
   {
-    title: "Wadan Real Estate",
+    title: "CorpComment",
     description:
       "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
     tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
